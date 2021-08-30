04ecd6e34c499545b4fefacafe7765e2288f0bfe
hayacon <hayacon13@gmail.com>
2021-08-30 22:43:28 +0100

    add open api schemas

--- SNS/urls.py
+++ SNS/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('swaggerdocs/', TemplateView.as_view(
+        template_name='snsApp/swagger-docs.html',
+        extra_context={"schema_url":"openapi-schema"}
+    ), name='swagger-docs'),
+    path('apischema/', get_schema_view(
+        title='snsApp REST API',
+        description='API for interact with user data and post',
+        version='1.0'
+    ), name='openapi-schema'),
     path('admin/', admin.site.urls),
     path('', include('snsApp.urls')),
 ]
